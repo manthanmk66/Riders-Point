6ef6145e05666458bcb5e23bee4831fbd2e4a4d9
Rohit Sutar <114717436+rohitsureshsutar2000@users.noreply.github.com>
2023-12-20 16:41:14 +0530

    Update RiderspointDB.sql

--- RiderspointDB.sql
+++ RiderspointDB.sql
@@ -14,7 +14,7 @@ drop table if exists pillion_route_details;
 	create table loginrider(id int,username varchar(20),password varchar(20) ,constraint fk_rd_pass_rid foreign 			key(id) references rider_details(rider_id));
 
 
-	create table pillion_details(pillion_id int primary key 				auto_increment,name varchar(30),mobile_no varchar(10),address 			varchar(100));
+	create table pillion_details(pillion_id int primary key auto_increment,name varchar(30),mobile_no varchar(10),address 			varchar(100));
 	create table status_pillion(status_id int primary key 					auto_increment,pillion_id int,avg_expense varchar(20),constraint 		fk_statusp_rid foreign key(pillion_id) references 				pillion_details(pillion_id));
 	create table pillion_route_details(route_id int not null unique,status_id 		int,start_point varchar(20), end_point varchar(20),travel_date 			date,description varchar(100),constraint fk_prd_rid foreign 			key(status_id) references status_pillion(status_id));
 	create table loginpillion(id int,username varchar(20),password varchar(20) ,constraint fk_rd_pass_rid foreign 			key(id) references pillion_details(pillion_id));
