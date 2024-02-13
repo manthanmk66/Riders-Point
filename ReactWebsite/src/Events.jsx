a86323db41c5c57989a016ac1b786907ea1d4c90
Manthan Kasle <manthankaslemk@gmail.com>
2024-02-13 22:07:24 +0530

    added

--- ReactWebsite/src/Events.jsx
+++ ReactWebsite/src/Events.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Events = () => {
   return (
     <>   <div>Events</div>
-    <h1 className='px-48 py-96'> This Section is For  Upcoming Events </h1>
+    <h1 className='px-48 py-96'> This Section is For  Upcoming Event </h1>
     </>
  
   )
